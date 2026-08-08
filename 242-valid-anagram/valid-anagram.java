1bb36a84803f87ae436a3aa195c0d2a331d4c420
sahilraj@930 <souryakhanna@gmail.com>
2026-08-08 21:26:19 +0530

    Time: 24 ms (5.24%) | Memory: 46.7 MB (9.31%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -4,6 +4,10 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         HashMap<Character, Integer> hm = new HashMap<>();
 
+        if(s.length() != t.length()){
+            return false;
+        }
+
         for(int i=0; i<s.length(); i++){
             char num = s.charAt(i);
             hm.put(num, hm.getOrDefault(num, 0) + 1);
@@ -21,6 +25,6 @@ class Solution {
             }
         }
 
-        return hm.isEmpty();
+        return true;
     }
 }
