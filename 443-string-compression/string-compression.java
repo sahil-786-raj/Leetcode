cc0b7f46ac1f9318d9420360cd067baa8de91287
sahilraj@930 <souryakhanna@gmail.com>
2026-08-13 13:28:29 +0530

    Time: 3 ms (10.79%) | Memory: 45.5 MB (49.46%) - LeetSync

---
+++ 443-string-compression/string-compression.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int compress(char[] chars) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<chars.length; i++){
+            Integer count = 1;
+            while(i<chars.length-1 && chars[i] == chars[i+1]){
+                count++;
+                i++;
+            }
+            sb.append(chars[i]);
+            if(count>1){
+                sb.append(count);
+            }
+        }
+
+        for(int i=0; i<sb.length(); i++){
+            chars[i] = sb.charAt(i);
+        }
+
+        return sb.length();
+    }
+}
