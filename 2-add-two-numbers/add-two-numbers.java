76fb028ee0ffa725c38062e4e25d869a9b9fcf6e
sahilraj@930 <souryakhanna@gmail.com>
2026-08-25 01:57:30 +0530

    Time: 1 ms (100.00%) | Memory: 46.8 MB (6.54%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -46,7 +46,6 @@ class Solution {
         if(carry != 0){
             ListNode newnode = new ListNode(carry);
             curr.next = newnode;
-            curr = curr.next;
         }
 
         return dummynode.next;
