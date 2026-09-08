c550b385256f5374ae0494680479505426fb880f
sahilraj@930 <souryakhanna@gmail.com>
2026-09-08 16:45:00 +0530

    Time: 4 ms (73.99%) | Memory: 45.5 MB (10.59%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,36 +1,25 @@
 class Solution {
-    public static int[] nextgreater(int[] nums2){
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Stack<Integer> s = new Stack<>();
-        int arr[] = new int[nums2.length];
+        HashMap<Integer, Integer> hm = new HashMap<>();
         for(int i=nums2.length-1; i>=0; i--){
-
             while(!s.isEmpty() && nums2[s.peek()] <= nums2[i]){
                 s.pop();
             }
-
             if(s.isEmpty()){
-                arr[i] = -1;
+                hm.put(nums2[i], -1);
             }else{
-                arr[i] = nums2[s.peek()];
+                hm.put(nums2[i], nums2[s.peek()]);
             }
 
             s.push(i);
         }
-        return arr;
-    }
-    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int arr[] = nextgreater(nums2);
-        int ans[] = new int[nums1.length];
-        
-        for(int i=0; i<nums1.length; i++){
-            int j = 0;
-            while(nums1[i] != nums2[j]){
-                j++;
-            }
 
-            ans[i] = arr[j];
+        int arr[] = new int[nums1.length];
+        for(int i=0; i<nums1.length; i++){
+            arr[i] = hm.get(nums1[i]);
         }
 
-        return ans;
+        return arr;
     }
 }
