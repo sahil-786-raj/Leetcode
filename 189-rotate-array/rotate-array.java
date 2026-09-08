768026d35b4719dea5daf33867f3d86cf32f0dee
sahilraj@930 <souryakhanna@gmail.com>
2026-09-08 17:37:45 +0530

    Time: 5 ms (19.42%) | Memory: 268.6 MB (21.43%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -10,7 +10,9 @@ class Solution {
     }
     public void rotate(int[] nums, int k) {
         int n = nums.length;
-        k = k%n;
+        if(k > n){
+            k = k%n;
+        }
         reverse(nums, 0, n-k-1);
         reverse(nums, n-k, n-1);
         reverse(nums, 0, n-1);
